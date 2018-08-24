34d47968f7f83e107544927c720e2d548d38a31a
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-23 18:49:40 -0700

    add Board#fill_grid

--- minesweeper/board.rb
+++ minesweeper/board.rb
@@ -8,6 +8,7 @@ class Board
 
   def populate
     place_bombs
+    fill_grid
   end
 
   def place_bombs
@@ -16,6 +17,20 @@ class Board
       pos = [rand(0...9), rand(0...9)]
       self[pos] = bombs.shift if self[pos] == nil
     end
+  end
+
+  def fill_grid
+    @grid.each_index do |row_i|
+      @grid[row_i].each_index do |col_i|
+        pos = [row_i, col_i]
+        self[pos] = generate_tile_for(row_i, col_i) if self[pos] == nil
+      end
+    end
+  end
+
+  def generate_tile_for(row_i, col_i)
+    bombs_adj = 0
+
   end
 
   def [](pos)
